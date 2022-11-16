61168fb57d407cb4045ad90f9b5e8859113cf7cf
Daniel Vilas Boas <dvbs15@hotmail.com>
2022-11-16 11:12:37 -0300

    Criando rotas do cadastro de pessoa juridica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CadastroPjRoutingModule } from './autenticacao/cadastro-pj/components/cadastro-pj-routing.module';
 import { LoginRoutingModule } from './autenticacao/login/login-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { LoginModule } from './autenticacao/login/login.module';
 import { AppRoutingModule } from './app-routing.module';
+import { CadastroPjModule } from './autenticacao/cadastro-pj/cadastro-pj.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,8 @@ import { AppRoutingModule } from './app-routing.module';
     MatButtonModule,
     LoginModule,
     LoginRoutingModule,
+    CadastroPjModule,
+    CadastroPjRoutingModule,
 
     AppRoutingModule
   ],
---
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastrarPjComponent } from './components/cadastrar-pj/cadastrar-pj.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+import { CadastroPjComponent } from './components/cadastro-pj.component';
+
+
+@NgModule({
+  declarations: [
+    CadastrarPjComponent,
+    CadastroPjComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule
+  ]
+})
+export class CadastroPjModule { }
