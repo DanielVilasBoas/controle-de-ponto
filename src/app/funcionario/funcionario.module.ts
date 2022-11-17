1d000e91501a43c574671673e3b8fce811aab259
Daniel Vilas Boas <dvbs15@hotmail.com>
2022-11-17 20:27:15 -0300

    Impementando componente de lançamento

--- src/app/funcionario/funcionario.module.ts
+++ src/app/funcionario/funcionario.module.ts
@@ -5,6 +5,17 @@ import { LancamentoComponent } from './components/lancamento/lancamento.componen
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FuncionarioComponent } from './components/funcionario.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -17,7 +28,18 @@ import { FuncionarioComponent } from './components/funcionario.component';
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule
   ]
 })
 export class FuncionarioModule { }
