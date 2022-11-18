f1fb5877c58ced6fac8e763b456a34ce830c6abf
Daniel Vilas Boas <dvbs15@hotmail.com>
2022-11-18 11:14:00 -0300

    Criando a listagem de lançamentos

--- src/app/funcionario/components/listagem/listagem.component.ts
+++ src/app/funcionario/components/listagem/listagem.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LancamentoService } from '../../../shared/services/lancamento.service';
+import { Lancamento } from '../../../shared/models/lancamento.model';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-listagem',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemComponent implements OnInit {
 
-  constructor() { }
+  dataSource: MatTableDataSource<Lancamento>;
+  colunas: string[] = ['data', 'tipo', 'localizacao'];
+
+  constructor(
+    private lancamentoService: LancamentoService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.lancamentoService.listarTodosLancamentos()
+    .subscribe(
+      data => {
+        const lancamentos = data['data'] as Lancamento[];
+        this.dataSource = new MatTableDataSource<Lancamento>(lancamentos);
+      },
+      err => {
+        const msg: string = "Erro obtendo lançamentos.";
+        this.snackBar.open(msg, "Erro", { duration: 5000 });
+      }
+    )
   }
 
 }
--- src/app/shared/services/lancamento.service.ts
+++ src/app/shared/services/lancamento.service.ts
@@ -11,9 +11,9 @@ import { Lancamento } from '../models/lancamento.model';
 export class LancamentoService {
 
   private readonly PATH: string = 'lancamentos';
-  private readonly PATH_ULTIMO_LANC = '/funcionario/{funcionarioID}/ultimo';
-  private readonly PATH_LANCAMENTOS = '/funcionario/{funcionarioID}';
-  private readonly PATH_TODOS_LANC = '/funcionario/{funcionarioID}/todos';
+  private readonly PATH_ULTIMO_LANC = '/funcionario/{funcionarioId}/ultimo';
+  private readonly PATH_LANCAMENTOS = '/funcionario/{funcionarioId}';
+  private readonly PATH_TODOS_LANC = '/funcionario/{funcionarioId}/todos';
 
   constructor(
     private http: HttpClient,
@@ -24,7 +24,7 @@ export class LancamentoService {
       return this.http.get(
         env.baseApiUrl + this.PATH +
         this.PATH_ULTIMO_LANC.replace(
-          '{funcionarioID}', this.httpUtil.obterIdUsuario()),
+          '{funcionarioId}', this.httpUtil.obterIdUsuario()),
           this.httpUtil.headers()
       );
     }
@@ -35,5 +35,14 @@ export class LancamentoService {
         lancamento,
         this.httpUtil.headers()
       );
+    }
+
+    listarTodosLancamentos(): Observable<any> {
+      return this.http.get(
+        env.baseApiUrl + this.PATH +
+        this.PATH_TODOS_LANC.replace(
+          '{funcionarioId}', this.httpUtil.obterIdUsuario()),
+          this.httpUtil.headers()
+      )
     }
 }
